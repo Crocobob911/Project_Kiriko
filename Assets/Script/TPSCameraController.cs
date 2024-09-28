65d5ab728434ffc3cda18a14a707c63f549ce751
Crocobob911 <mjdaniel911@naver.com>
2024-09-28 23:41:37 +0900

    Player Look At Camera & WASD move
    카메라 방향으로 플레이어 회전
    WASD 움직임 (아직 애니메이션 없음)

--- Assets/Script/TPSCameraController.cs
+++ Assets/Script/TPSCameraController.cs
@@ -8,16 +8,16 @@ public class TPSCameraController : MonoBehaviour
     private Transform cameraRoot;
     [SerializeField, Range(0, 10)]
     float sensitivity = 1f;  // 시야 회전 감도
+    [SerializeField, Range(0f, 60f)]
+    private float camAngleMaximum;  //카메라가 어디까지 올라가나. 값이 60 이하일 것!
+    [SerializeField, Range(270f, 361f)]
+    float camAngleMinimum;  //카메라가 어디까지 내려가나.
 
     Vector2 mouseMoveDelta = new Vector2(0, 0);  //마우스 이동 입력
-    float camAngleX_adjusted; //카메라 수직 이동 범위 제한
     Vector3 camAngle = new Vector3(0, 0, 0);  //카메라 최종 위치
+    float camAngleX_adjusted; //카메라 수직 이동 범위 제한
 
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
     void Update()
     {
         LookAround();
@@ -25,21 +25,21 @@ public class TPSCameraController : MonoBehaviour
 
     private void LookAround()
     {
+        //마우스 입력
         mouseMoveDelta.x = Input.GetAxis("Mouse X");
         mouseMoveDelta.y = Input.GetAxis("Mouse Y");
 
+        //카메라 위치값 선계산
         camAngle = cameraRoot.rotation.eulerAngles;
 
+        //카메라 이동 범위 제한
         camAngleX_adjusted = camAngle.x - mouseMoveDelta.y * sensitivity;
-        if (camAngleX_adjusted < 180f)
-        {
-            camAngleX_adjusted = Mathf.Clamp(camAngleX_adjusted, -1f, 30f);
-        }
-        else
-        {
-            camAngleX_adjusted = Mathf.Clamp(camAngleX_adjusted, 225f, 361f);
-        }
+        if (camAngleX_adjusted < 180f) //카메라가 수평선 위
+            camAngleX_adjusted = Mathf.Clamp(camAngleX_adjusted, -1f, camAngleMaximum);
+        else //카메라가 수평선 아래
+            camAngleX_adjusted = Mathf.Clamp(camAngleX_adjusted, camAngleMinimum, 361f);
 
+        //카메라 위치 변경
         cameraRoot.rotation = Quaternion.Euler(
             camAngleX_adjusted,
             camAngle.y + mouseMoveDelta.x * sensitivity,
--- Assets/Script/TPSPlayerController.cs
+++ Assets/Script/TPSPlayerController.cs
@@ -6,6 +6,14 @@ public class TPSPlayerController : MonoBehaviour
 {
     [SerializeField]
     private Transform playerBody;
+    [SerializeField]
+    private Transform cameraRoot;
+    [SerializeField]
+    float moveSpeed;
+
+    bool isMoveInput = false;
+    private Vector2 moveInput = new Vector2(0,0);
+    private Vector3 playerLookAt = new Vector3(0,0,0);
 
     Animator anim;
 
@@ -18,6 +26,22 @@ public class TPSPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
 
+    private void Move()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        isMoveInput = moveInput.magnitude != 0;
+        anim.SetBool("isMove", isMoveInput);
+        if (isMoveInput) {
+            Vector3 lookForward = new Vector3(cameraRoot.forward.x, 0f, cameraRoot.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraRoot.right.x, 0f, cameraRoot.forward.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            playerBody.forward = lookForward;
+            transform.position += moveDir * moveSpeed * Time.deltaTime ;
+        }
+        playerLookAt = new Vector3(cameraRoot.forward.x, 0f, cameraRoot.forward.z);
     }
 }
