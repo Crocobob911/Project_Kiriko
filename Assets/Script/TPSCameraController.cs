5c59a76ea0adfd31fa60d2a52d2f5a53e69ccf4d
Crocobob <mjdaniel911@naver.com>
2024-10-24 02:29:23 +0900

    Cam Zoom, Player Run
    스크롤로 카메라 줌인 줌아웃
    카메라 리팩터링은 끝난 듯...? |
    쉬프트 눌러서 달리기 |
    대각선 빨라짐 수정 필요 |
    브랜치도 좀 정리하자

--- Assets/Script/TPSCameraController.cs
+++ Assets/Script/TPSCameraController.cs
@@ -1,156 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using OpenCover.Framework.Model;
 using UnityEngine;
 
 public class TPSCameraController : MonoBehaviour
 {
-    [SerializeField]
-    TPSPlayerController playerController;
-    [SerializeField]
-    private Transform cameraRoot;
+    [SerializeField] private PlayMoveController playerController;
+    [SerializeField] private Transform cameraRoot;
 
-    Vector2 mouseMoveDelta = new Vector2(0, 0);  //마우스 이동 입력
-    bool isCamLocked = false;
+    private Vector2 mouseMoveDelta = new(0, 0);
+    private bool isCamLocked;
 
-    readonly float camLockFindDistance = 100f;
-    [SerializeField, Header("Lock On 인식 너비, *부채꼴 아님.")]     
-    float camLockFindRadius = 5f;  //정확하게 부채꼴 범위로 찾을 순 없을까?
+    private float camLockFindDistance = 100f;
 
-    GameObject lockedObj; //록온한 오브젝트
+    [SerializeField, Header("Lock On find radius")]
+    private float camLockFindRadius = 5f;
 
-    RaycastHit[] hits; //시점 방향의 모든 오브젝트를 다 담음   
-    ICameraRotationCalculator camCal;
-    UnLockedCamCalculator unLockedCam;
-    LockedCamCalculator lockedCam;
+
+    private GameObject lockedObj;
+
+    private RaycastHit[] hits;
+    private ICameraRotationCalculator camCal;
+    private UnLockedCamCalculator unLockedCam;
+    private LockedCamCalculator lockedCam;
 
     private void Start()
     {
         unLockedCam = new UnLockedCamCalculator(cameraRoot);
         lockedCam = new LockedCamCalculator(playerController.gameObject);
         camCal = unLockedCam;
-        Debug.Log(camCal);
     }
 
     void Update()
     {
-        Debug.DrawRay(cameraRoot.position, 
+        Debug.DrawRay(cameraRoot.position,
             new Vector3(cameraRoot.forward.x, 0, cameraRoot.forward.z));
         TriggerCamLock();
         ChangeCameraRotation();
     }
 
-    private void ChangeCameraRotation() //Camera Lock On?? ??? ??   **** ???? ?????? if ???????...??? ?????
+    private void ChangeCameraRotation()
     {
-        //???²J ???
         mouseMoveDelta.x = Input.GetAxis("Mouse X");
         mouseMoveDelta.y = Input.GetAxis("Mouse Y");
 
         cameraRoot.rotation = camCal.SetCameraRotation(mouseMoveDelta);
     }
 
-    private void TriggerCamLock()  //1 ?????? Cam lock ?????
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void TriggerCamLock()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (!Input.GetKeyDown(KeyCode.Alpha1)) return;
+        if (isCamLocked)
         {
-            if (isCamLocked)  //Cam Lock On ????
-            {
-                isCamLocked = false;
-                camCal.SetLockedObj(null);
-                camCal = unLockedCam;
+            isCamLocked = false;
+            camCal.SetLockedObj(null);
+            camCal = unLockedCam;
 
-                playerController.CamLock(isCamLocked);
-            }
-            else if (lockedObj = FindObjectToLock()) //Enemy?? ?????????? ???
-            {
-                isCamLocked = true;
-                camCal = lockedCam;
-                camCal.SetLockedObj(lockedObj);
-                playerController.CamLock(isCamLocked);
-            }
-            else //Enemy ?????
-            {
-                camCal = unLockedCam;
-                Debug.Log("No Enemy To Lock On");
-            }
+            playerController.CamLock(isCamLocked);
+        }
+        // ReSharper disable once AssignmentInConditionalExpression
+        else if (lockedObj = FindObjectToLock())
+        {
+            isCamLocked = true;
+            camCal = lockedCam;
+            camCal.SetLockedObj(lockedObj);
+            playerController.CamLock(isCamLocked);
+        }
+        else
+        {
+            camCal = unLockedCam;
+            Debug.Log("No Enemy To Lock On");
         }
     }
 
-    private GameObject FindObjectToLock()  //RaySphere?? ????, ???? ??? ?????? Enemy?? ???
+    private GameObject FindObjectToLock()
     {
-        hits = Physics.SphereCastAll(cameraRoot.position, camLockFindRadius, 
-            new Vector3(cameraRoot.forward.x, 0, cameraRoot.forward.z), 
-                camLockFindDistance);
-            foreach (RaycastHit hit in hits) { 
-            if(hit.transform.GetComponent<Enemy>()) //?? ????? Enemy?? ???? ??????? ????? ?????. ???? ?????? ??????? Enemy?? ??? ??? ??????? ???.
-            {
-                return hit.transform.GetComponent<Enemy>().transform.gameObject;
-            }
-        }
-        return null;
+        // ReSharper disable once Unity.PreferNonAllocApi
+        hits = Physics.SphereCastAll(cameraRoot.position, camLockFindRadius,
+            new Vector3(cameraRoot.forward.x, 0, cameraRoot.forward.z),
+            camLockFindDistance);
+        return (from hit in hits
+                where hit.transform.GetComponent<Enemy>()
+                select hit.transform.GetComponent<Enemy>().transform.gameObject).FirstOrDefault();
     }
 }
 
-// ??? or ????? ??, ???? ???? ???? ??????? ???? ??? ???
-interface ICameraRotationCalculator
-{
-    abstract public Quaternion SetCameraRotation(Vector2 mouseMoveDelta);
-    abstract public void SetLockedObj(GameObject obj);
+internal interface ICameraRotationCalculator {
+    public Quaternion SetCameraRotation(Vector2 mouseMoveDelta);
+    public void SetLockedObj(GameObject obj);
 }
 
-public class LockedCamCalculator : ICameraRotationCalculator
-{
+public class LockedCamCalculator : ICameraRotationCalculator {
     private GameObject lockedObj;
     private GameObject playerObj;
 
-    public LockedCamCalculator(GameObject player)
-    {
+    public LockedCamCalculator(GameObject player) {
         playerObj = player;
     }
 
-    public void SetLockedObj(GameObject obj)
-    {
+    public void SetLockedObj(GameObject obj) {
         lockedObj = obj;
     }
 
-    public Quaternion SetCameraRotation(Vector2 mouseMoveDelta)
-    {
+    public Quaternion SetCameraRotation(Vector2 mouseMoveDelta) {
         return Quaternion.LookRotation(lockedObj.transform.position - playerObj.transform.position);
     }
 }
 
-public class UnLockedCamCalculator : ICameraRotationCalculator
-{
-    [SerializeField, Range(0, 10)] float sensitivity = 1f;  // ??­? ??? ????
-    [SerializeField, Range(0f, 60f)] float camAngleMaximum = 40f;  //???? ?????? ????. ???? 60 ?????? ??!
-    [SerializeField, Range(270f, 361f)] float camAngleMinimum = 300f;  //???? ?????? ????????.
-    private float camAngleX_adjusted; //???? ???? ??? ???? ????
+public class UnLockedCamCalculator : ICameraRotationCalculator {
+    [SerializeField, Range(0, 10)] float sensitivity = 1f;
+    [SerializeField, Range(0f, 60f)] float camAngleMaximum = 40f;
+    [SerializeField, Range(270f, 361f)] float camAngleMinimum = 300f;
+    private float camAngleXadjusted;
 
     private Transform cameraRoot;
     private Vector3 camAngle;
-
-    public UnLockedCamCalculator(Transform root)
-    {
+    
+    public UnLockedCamCalculator(Transform root) {
         cameraRoot = root;
     }
+    
+    /// Get 'how much the mouse moved' and return 'how much the camera should move'
+    /// It is executed when the Cam Locked off
+    public Quaternion SetCameraRotation(Vector2 mouseMoveDelta) {
+        camAngle = cameraRoot.rotation.eulerAngles;
+        camAngleXadjusted = camAngle.x - mouseMoveDelta.y * sensitivity;
+        camAngleXadjusted = 
+            camAngleXadjusted < 180f ? Mathf.Clamp(camAngleXadjusted, -1f, camAngleMaximum) 
+                : Mathf.Clamp(camAngleXadjusted, camAngleMinimum, 361f);
 
-    public Quaternion SetCameraRotation(Vector2 mouseMoveDelta)
-    {
-        camAngle = cameraRoot.rotation.eulerAngles;         //???? ????? ?????
-        camAngleX_adjusted = camAngle.x - mouseMoveDelta.y * sensitivity;
-        if (camAngleX_adjusted < 180f) //???? ???? ??
-        {
-            camAngleX_adjusted = 
-            Mathf.Clamp(camAngleX_adjusted, -1f, camAngleMaximum);
-        }
-        else //???? ???? ???
-        {
-            camAngleX_adjusted = 
-            Mathf.Clamp(camAngleX_adjusted, camAngleMinimum, 361f);
-        }
-
-        return Quaternion.Euler(camAngleX_adjusted, 
+        return Quaternion.Euler(camAngleXadjusted, 
             camAngle.y + mouseMoveDelta.x * sensitivity, camAngle.z);
     }
-
-    public void SetLockedObj(GameObject obj) { }  //??? ???????
+    
+    public void SetLockedObj(GameObject obj) { }
 }
